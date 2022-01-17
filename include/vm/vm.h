e4783ce3bb417f8c0d76b8733f88727007c6c6d2
woonys <wodns1324@naver.com>
2022-01-17 14:12:33 +0000

    [Project 3-1] page_hash member

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -46,7 +46,8 @@ struct page {
 	struct frame *frame;   /* Back reference for frame */
 
 	/* Your implementation */
-
+	/* --- Project 3: VM-SPT ---*/
+	struct hash_elem hash_elem;
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
 	union {
@@ -85,6 +86,7 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash spt_hash;
 };
 
 #include "threads/thread.h"
