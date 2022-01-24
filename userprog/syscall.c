96a9d46ac39da279f56d35aba9581997664eaa02
woonys <wodns1324@naver.com>
2022-01-24 19:32:07 +0000

    mmap &munmap syscall 수정

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -168,6 +168,18 @@ syscall_handler (struct intr_frame *f UNUSED) {
 			close(f->R.rdi);
 			break;
 		}
+
+		case SYS_MMAP:
+		{
+			f->R.rax = mmap(f->R.rdi, f->R.rsi, f->R.rdx, f->R.r10, f->R.r8);
+			break;
+		}
+		
+		case SYS_MUNMAP:
+		{
+			munmap(f->R.rdi);
+			break;
+		}
 			
 		default:
 			exit(-1);
